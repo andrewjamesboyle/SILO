3a5caba01da6ca6902305682d45e8405ab4e31de
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-08-22 09:56:17 -0700

    tailwind CSS

--- web/src/components/ProcessData/ProcessData.tsx
+++ web/src/components/ProcessData/ProcessData.tsx
@@ -19,7 +19,23 @@ type DataTable =
 
 const ProcessDataComponent: React.FC = () => {
   const [selectedTable, setSelectedTable] = useState<DataTable | null>(null)
-  const [uploadedFile, setUploadedFile] = useState<string | null>(null)
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null)
+
+  const dataTables: DataTable[] = [
+    'mons',
+    'control',
+    'wetlands',
+    'testpit',
+    'soilbore',
+    'boundary',
+    'boundarystb',
+    'ordhwtr',
+    'ordhwtrstb',
+    'meanhighwtr',
+    'wetlandsln',
+    'wetlandslnbuf',
+    'misc',
+  ]
 
   const handleProcessClick = async () => {
     if (!selectedTable || !uploadedFile) {
@@ -27,7 +43,7 @@ const ProcessDataComponent: React.FC = () => {
       return
     }
 
-    const filename = uploadedFile.split(/[\\\/]/).pop()
+    const filename = uploadedFile.name
     const baseUrl = `http://146.190.37.102:5000/process?tbl=${selectedTable}&dataset=${filename}`
 
     try {
@@ -41,126 +57,25 @@ const ProcessDataComponent: React.FC = () => {
   return (
     <div>
       <div>
-        <label>
-          <input
-            type="radio"
-            value="mons"
-            checked={selectedTable === 'mons'}
-            onChange={() => setSelectedTable('mons')}
-          />
-          Mons
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="control"
-            checked={selectedTable === 'control'}
-            onChange={() => setSelectedTable('control')}
-          />
-          Control
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="wetlands"
-            checked={selectedTable === 'wetlands'}
-            onChange={() => setSelectedTable('wetlands')}
-          />
-          Wetlands
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="testpit"
-            checked={selectedTable === 'testpit'}
-            onChange={() => setSelectedTable('testpit')}
-          />
-          Testpit
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="soilbore"
-            checked={selectedTable === 'soilbore'}
-            onChange={() => setSelectedTable('soilbore')}
-          />
-          Soilbore
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="boundary"
-            checked={selectedTable === 'boundary'}
-            onChange={() => setSelectedTable('boundary')}
-          />
-          Boundary
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="boundarystb"
-            checked={selectedTable === 'boundarystb'}
-            onChange={() => setSelectedTable('boundarystb')}
-          />
-          Boundarystb
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="ordhwtr"
-            checked={selectedTable === 'ordhwtr'}
-            onChange={() => setSelectedTable('ordhwtr')}
-          />
-          Ordhwtr
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="ordhwtrstb"
-            checked={selectedTable === 'ordhwtrstb'}
-            onChange={() => setSelectedTable('ordhwtrstb')}
-          />
-          Ordhwtrstb
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="meanhighwtr"
-            checked={selectedTable === 'meanhighwtr'}
-            onChange={() => setSelectedTable('meanhighwtr')}
-          />
-          Meanhighwtr
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="wetlandsln"
-            checked={selectedTable === 'wetlandsln'}
-            onChange={() => setSelectedTable('wetlandsln')}
-          />
-          Wetlandsln
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="wetlandslnbuf"
-            checked={selectedTable === 'wetlandslnbuf'}
-            onChange={() => setSelectedTable('wetlandslnbuf')}
-          />
-          Wetlandslnbuf
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="misc"
-            checked={selectedTable === 'misc'}
-            onChange={() => setSelectedTable('misc')}
-          />
-          Misc
-        </label>
+        {dataTables.map((table) => (
+          <label key={table}>
+            <input
+              type="radio"
+              value={table}
+              checked={selectedTable === table}
+              onChange={() => setSelectedTable(table)}
+            />
+            {table.charAt(0).toUpperCase() + table.slice(1)}
+          </label>
+        ))}
       </div>
       <div>
-        <input type="file" onChange={(e) => setUploadedFile(e.target.value)} />
+        <input
+          type="file"
+          onChange={(e) =>
+            setUploadedFile(e.target.files ? e.target.files[0] : null)
+          }
+        />
       </div>
       <button onClick={handleProcessClick}>Process</button>
     </div>
