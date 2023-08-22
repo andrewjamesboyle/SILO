ced6163596a20ce35f1defffc6baaf895cc8cceb
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-08-22 10:08:07 -0700

    map layout and storybook setup

---
+++ web/src/layouts/MapLayout/MapLayout.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MapLayout from "./MapLayout";
+
+const meta: Meta<typeof MapLayout> = {
+  component: MapLayout,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MapLayout>;
+
+export const Primary: Story = {};
---
+++ web/src/layouts/MapLayout/MapLayout.tsx
@@ -0,0 +1,9 @@
+type MapLayoutProps = {
+  children?: React.ReactNode;
+};
+
+const MapLayout = ({ children }: MapLayoutProps) => {
+  return <>{children}</>;
+};
+
+export default MapLayout;
