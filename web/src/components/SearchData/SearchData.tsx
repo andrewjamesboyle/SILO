2bae0a74a72c5986b4e65bd813c558b242134b0a
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-10-26 20:11:03 -0700

    update search bar and property type select

--- web/src/components/SearchData/SearchData.tsx
+++ web/src/components/SearchData/SearchData.tsx
@@ -46,7 +46,7 @@ const SearchDataComponent: React.FC = () => {
         />
         <input
           id="search-field"
-          className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+          className="block h-full w-full m-4 border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 text-xl"
           placeholder="Search..."
           type="search"
           name="search"
@@ -54,9 +54,9 @@ const SearchDataComponent: React.FC = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </form>
-      <div className="flex items-center gap-x-4 lg:gap-x-6">
+      {/* <div className="flex items-center gap-x-4 lg:gap-x-6">
         {/* Render input elements for `aoi` and `search` here */}
-        <label>
+      {/* <label>
           <select value={aoi} onChange={(e) => setAoi(e.target.value)}>
             <option value="" disabled>
               Select Area of Interest
@@ -65,6 +65,28 @@ const SearchDataComponent: React.FC = () => {
             <option value="address">Address</option>
           </select>
         </label>
+      </div> */}
+      <div className="flex items-center justify-evenly gap-x-4 lg:gap-x-6 bg-gray-300 pl-1 pr-1 pt-1 pb-1 rounded-xl w-full">
+        <button
+          className={`${
+            aoi === 'parcel'
+              ? 'bg-white text-black'
+              : 'bg-gray-300 text-gray-700'
+          } py-2 px-4 rounded-md w-1/2`}
+          onClick={() => setAoi('parcel')}
+        >
+          Parcel
+        </button>
+        <button
+          className={`${
+            aoi === 'address'
+              ? 'bg-white text-black'
+              : 'bg-gray-300 text-gray-700'
+          } py-2 px-4 rounded-md w-1/2`}
+          onClick={() => setAoi('address')}
+        >
+          Address
+        </button>
       </div>
     </>
   )
