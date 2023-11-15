63cb9662160caa5f13dc330a9853f6bfefed352a
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-11-15 10:14:30 -0800

    placeholder commit while in progress - UI for landing page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -14,20 +14,20 @@ export default function Home() {
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
+      <header className="absolute inset-x-0 top-0 z-50 justify-between">
         <nav
           className="flex items-center justify-center p-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">SILO</span>
               <h1 className="text-3xl font-extrabold leading-9 text-gray-900">
                 SILO
               </h1>
             </a>
           </div>
-          <div className="flex lg:hidden">
+          <div className="flex lg:hidden justify-between">
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -67,7 +67,7 @@ export default function Home() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">SILO</span>
                 <img
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
@@ -110,23 +110,59 @@ export default function Home() {
         </Dialog>
       </header>
 
+      {/* <header className="absolute inset-x-0 top-0 z-50 flex items-center justify-between p-6 lg:px-8"> */}
+      {/* SILO on the left corner */}
+      {/* <div>
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">SILO</span>
+            <h1 className="text-3xl font-extrabold leading-9 text-gray-900">
+              SILO
+            </h1>
+          </a>
+        </div> */}
+
+      {/* Hamburger menu on the right corner */}
+      {/* <div className="lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+      </header> */}
+
+      {/* <div
+        className="
+        fixed
+        bg-gradient-to-r from-[#ff80b5] to-[#9089fc]
+        h-24
+        w-24
+        left-2
+        top-2
+        z-0
+      "
+      /> */}
+
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
-          <div
+          {/* <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          />
+          /> */}
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
+              SILO is everything you need for custom maps and data layers.{' '}
               <a href="" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -135,7 +171,7 @@ export default function Home() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Create custom maps using our cutting-edge geospatial API
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
