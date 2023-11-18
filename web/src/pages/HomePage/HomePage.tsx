bd4f19699ace6295cb2d51d49fa81270ccaa7a67
Ryan J. Parker <ryan.j.parker@wsu.edu>
2023-11-18 15:36:22 -0800

    update home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -15,19 +15,22 @@ export default function Home() {
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50 justify-between">
         <nav
-          className="flex items-center justify-center p-6 lg:px-8"
+          className="flex items-center justify-end p-6 lg:px-8"
           aria-label="Global"
         >
-          <div className="flex lg:hidden justify-between">
+          {/* Hamburger icon */}
+          <div className="flex lg:hidden justify-end">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
+              className="-m-2.5 inline-flex items-center justify-end rounded-md p-2.5 text-gray-200"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-10 w-10" aria-hidden="true" />
             </button>
           </div>
+
+          {/* Navigation items at top left -> large viewport */}
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a
@@ -39,6 +42,7 @@ export default function Home() {
               </a>
             ))}
           </div>
+          <p className="border-t-4 border-gray-900 py-3" />
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
               href="/sign-in"
@@ -48,6 +52,8 @@ export default function Home() {
             </a>
           </div>
         </nav>
+
+        {/* Indigo sidebar when hamburger menu clicked */}
         <Dialog
           as="div"
           className="lg:hidden"
@@ -55,7 +61,7 @@ export default function Home() {
           onClose={setMobileMenuOpen}
         >
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-indigo-600 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <h1 className="text-3xl font-extrabold leading-9 text-gray-100">
@@ -78,16 +84,17 @@ export default function Home() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
                     </a>
                   ))}
                 </div>
+                <div className="border-t-4 border-gray-900 py-3" />
                 <div className="py-6">
                   <a
                     href="/sign-in"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-100 hover:text-gray-900 hover:bg-gray-50"
                   >
                     Log in
                   </a>
@@ -124,7 +131,7 @@ export default function Home() {
             </h2>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href=""
+                href="/sign-in"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Log in / Register
